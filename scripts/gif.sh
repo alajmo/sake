eb3d941880143f0389c6f4013296fdde4fab9fce
samiralajmovic <alajmovic.samir@gmail.com>
2022-06-14 18:45:54 +0200

    wip

--- scripts/gif.sh
+++ scripts/gif.sh
@@ -34,20 +34,20 @@ _simulate_commands() {
     _mock "# Ping all hosts one by one"
     sleep 1s
     _mock "sake run ping --tags remote"
-    sleep 2s
+    sleep 1.3s
     clear
 
     _mock "# Ping all hosts in parallel"
     sleep 1s
     _mock "sake run ping --tags remote --parallel"
-    sleep 2s
+    sleep 1.3s
     clear
 
     sleep 1s
     _mock "# Query servers for some stats"
     sleep 1s
     _mock "sake run info --all --output table"
-    sleep 4s
+    sleep 2s
     clear
   '
 
